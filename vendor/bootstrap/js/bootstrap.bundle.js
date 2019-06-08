81d254aa5f8e637c9701a74166f3a1fb84169bd6
akurz17 <akurz17@lvcp.org>
2019-06-07 22:57:45 -0400

    adding jq code to more .js files to get submenu to work

--- vendor/bootstrap/js/bootstrap.bundle.js
+++ vendor/bootstrap/js/bootstrap.bundle.js
@@ -4875,6 +4875,23 @@
         }
       };
 
+
+
+      $('.dropdown-submenu > a').on("click", function(e) {
+          var submenu = $(this);
+          $('.dropdown-submenu .dropdown-menu').removeClass('show');
+          submenu.next('.dropdown-menu').addClass('show');
+          e.stopPropagation();
+      });
+
+      $('.dropdown').on("hidden.bs.dropdown", function() {
+          // hide any open menus when parent closes
+          $('.dropdown-menu.show').removeClass('show');
+      });
+
+
+
+
       _proto._getScrollbarWidth = function _getScrollbarWidth() {
         // thx d.walsh
         var scrollDiv = document.createElement('div');
--- vendor/bootstrap/js/bootstrap.js
+++ vendor/bootstrap/js/bootstrap.js
@@ -2333,6 +2333,22 @@
         }
       };
 
+
+
+      $('.dropdown-submenu > a').on("click", function(e) {
+          var submenu = $(this);
+          $('.dropdown-submenu .dropdown-menu').removeClass('show');
+          submenu.next('.dropdown-menu').addClass('show');
+          e.stopPropagation();
+      });
+
+      $('.dropdown').on("hidden.bs.dropdown", function() {
+          // hide any open menus when parent closes
+          $('.dropdown-menu.show').removeClass('show');
+      });
+
+
+
       _proto._resetScrollbar = function _resetScrollbar() {
         // Restore fixed content padding
         $$$1(Selector.FIXED_CONTENT).each(function (index, element) {
